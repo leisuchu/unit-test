9f380cf8fa2d2cec56acc7f78661eec5b856b8e0
leisuchu <57219824+leisuchu@users.noreply.github.com>
2024-12-13 07:39:48 +0000

    feat():add方法完善

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { add } from 'lodash'
 
-export const add = (add1,add2) => {
-    return add(add1,add2)
+const NanToZero = (num) => {
+   return Number.isNaN(num)?0:num
+}
+export const add = (...adds) => {
+    return add(...adds.map(item => {
+        if(!item){
+            return 0
+        }
+
+        if(typeof item === 'string'){
+            return NanToZero(Number(item))
+        }else{
+            return NanToZero(item)
+        }
+    }))
 }
--- test/add.test.js
+++ test/add.test.js
@@ -3,4 +3,11 @@ import { add } from '../src/index'
 
 test('Add 方法',() => {
     expect(add(1,2)).toBe(3)
+    expect(add('1','2')).toBe(3)
+    expect(add('1',2)).toBe(3)
+    expect(add(1,'2')).toBe(3)
+    expect(add(null,2)).toBe(2)
+    expect(add(undefined,2)).toBe(2)
+    expect(add(1,null)).toBe(1)
+    expect(add(1,'test')).toBe(1)
 })
